9c93bc6e319eaf9b2e8601d7f0ccbbe41cc7d801
JeanS <jean.sinche@alarcongroup.us>
2024-12-26 23:02:35 -0500

    Implements fetching and mapping of now playing movies
    Fetches now playing movies from The Movie Database API and maps the results to a standardized Movie interface.  This involves creating new interfaces for movies and API responses, as well as a mapper class to handle the transformation.  The movie data is now returned as an array of `Movie` objects.

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,9 +1,12 @@
 import { movieApi } from "@/core/api/movie-api";
+import { MovieDBMoviesResponse } from "@/infrastructure/interfaces/moviedb-movies.response";
+import { MovieMapper } from "@/infrastructure/mappers/movie.mapper";
 
 export const nowPlayingAction = async () => {
   try {
-    const { data } = await movieApi.get("/now_playing");
-    console.log(data);
+    const { data } = await movieApi.get<MovieDBMoviesResponse>("/now_playing");
+    const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+    return movies;
   } catch (error) {
     console.log(error);
     throw "Now Playing Action Error";
--- core/api/movie-api.ts
+++ core/api/movie-api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const apiClient = axios.create({
+export const movieApi = axios.create({
   baseURL: process.env.EXPO_PUBLIC_MOVIE_DB_URL,
   params: {
     language: "es-MX",
   },
 });
 
-apiClient.defaults.headers.common[
+movieApi.defaults.headers.common[
   "Authorization"
 ] = `Bearer ${process.env.EXPO_PUBLIC_MOVIE_DB_ACCESS_TOKEN}`;
