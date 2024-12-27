d73ec102f669491435ebdd12ab651f19bf5b8ff0
JeanS <jean.sinche@alarcongroup.us>
2024-12-26 22:24:34 -0500

    prettier format

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import '../global.css'
+import { View, Text } from "react-native";
+import React from "react";
+import "../global.css";
 const RootLayout = () => {
   return (
     <View>
       <Text>RootLayout</Text>
     </View>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
